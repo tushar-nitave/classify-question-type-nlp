2d8b467cd1de17c7e13ff0e9e47a05422aab5778
tushar-nitave <tushar.nitave@gmail.com>
2018-01-10 17:09:19 +0530

    Trained the model with accuracy of 78%

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Y = dataset.iloc[:,1].values
 #categorizing the types of questions
 from sklearn.preprocessing import LabelEncoder
 labelencoder_y = LabelEncoder()
-Y = labelencoder_y.fit_transform(Y)
+y = labelencoder_y.fit_transform(Y)
 
 
 
@@ -25,8 +25,35 @@ import re
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-questions = re.sub('[^a-zA-z]', ' ', X[0][0])
-questions = questions.lower()
-questions = questions.split()
-questions = [word for word in questions if word in set(stopwords.words('english'))]
-questions = ' '.join(questions)
+corpus = []
+for i in range(0, 1483):
+    questions = re.sub('[^a-zA-z]', ' ', X[0][i])
+    questions = questions.lower()
+    questions = questions.split()
+    questions = [word for word in questions if word in set(stopwords.words('english'))]
+    questions = ' '.join(questions)
+    corpus.append(questions)
+    
+#creating bag of words
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+X = cv.fit_transform(corpus).toarray()
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)

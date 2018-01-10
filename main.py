f85e2afd80724f5c9a2bae3092fc0d7835f654fd
tushar-nitave <tushar.nitave@gmail.com>
2018-01-10 15:20:06 +0530

    encoding the categorical data

--- main.py
+++ main.py
@@ -8,6 +8,25 @@ import matplotlib.pyplot as plt
 #importing dataset
 dataset = pd.read_csv('data.tsv', delimiter=',,,', quoting=3)
 
+X = dataset.iloc[:,:-1].values
+X = pd.DataFrame(data=X)
+Y = dataset.iloc[:,1].values
+
+
+#categorizing the types of questions
+from sklearn.preprocessing import LabelEncoder
+labelencoder_y = LabelEncoder()
+Y = labelencoder_y.fit_transform(Y)
+
+
+
 #cleaning the question: removing punctuation marks
 import re
-questions = re.sub('[^a-zA-z]', ' ', dataset['question'][0])
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+questions = re.sub('[^a-zA-z]', ' ', X[0][0])
+questions = questions.lower()
+questions = questions.split()
+questions = [word for word in questions if word in set(stopwords.words('english'))]
+questions = ' '.join(questions)
